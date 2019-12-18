ae3d5545f794a0e963cd92e1aa59c6f0c3dedf39
独孤九剑 <1845230069@qq.com>
2019-12-18 20:51:53 +0800

    黑马头条PC-登录模块-页面布局及样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,30 @@
 <template>
-    <div>
-        登录页面
+    <div class="login">
+        <el-card>
+            <div class="title">
+                <img src="../../assets/logo_index.png">
+            </div>
+            <el-form style="margin-top:20px">
+                <el-form-item>
+                    <!-- 手机号 -->
+                    <el-input placeHolder="请输入手机号"></el-input>
+                </el-form-item>
+            <el-form-item>
+                 <!-- 验证码 -->
+                 <el-input style="width:60%" placeHolder="请输入验证码" ></el-input>
+                 <!-- 发送验证码 -->
+                 <el-button style="float:right">发送验证码</el-button>
+             </el-form-item>
+              <el-form-item>
+                 <!-- 同意选项 -->
+                 <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+             </el-form-item>
+              <el-form-item>
+                  <!-- 登录按钮 -->
+                  <el-button type="primary" style="width:100%">登录</el-button>
+              </el-form-item>
+              </el-form>
+        </el-card>
     </div>
 </template>
 
@@ -10,6 +34,23 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+    .login {
+            background-image: url('../../assets/login_bg.jpg');
+            background-size: cover ;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            .login-card {
+                width: 440px;
+                height: 350px;
+                .title {
+                    text-align : center;
+                    img {
+                         height:45px;
+                    }
+                }
+            }
+    }
 </style>
