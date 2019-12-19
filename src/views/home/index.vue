a0463bcb42bfce9f72f91ddc0b2335a4068ebfc9
独孤九剑 <1845230069@qq.com>
2019-12-19 17:35:01 +0800

    黑马头条PC-主页模块-页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,18 @@
 <template>
-  <div>
-      主页
-  </div>
+  <el-container>
+      <el-aside>左侧</el-aside>
+      <!-- 上下 -->
+      <el-container>
+          <el-header>
+            <!-- 头部 -->
+            头部
+          </el-header>
+          <!-- 主要区域 -->
+          <el-main>
+            主要区域
+          </el-main>
+      </el-container>
+  </el-container>
 </template>
 
 <script>
