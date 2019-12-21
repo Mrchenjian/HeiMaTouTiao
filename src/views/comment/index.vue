3bdf0a6b33ae9d03e5505b9e84319d5101bbd333
独孤九剑 <1845230069@qq.com>
2019-12-22 00:00:13 +0800

    黑马头条PC-评论列表-打开和关闭评论

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -5,20 +5,47 @@
                 <!-- 插槽内容 -->
                 <template slot="title">评论列表</template>
                 </bread-crumb>
-                <el-table>
+                <el-table :data='list'>
                 <!-- 列组件 label 表头-->
-                <el-table-column  width="600" label="标题"></el-table-column>
-                <el-table-column label="评论状态"></el-table-column>
-                <el-table-column label="总评论数"></el-table-column>
-                <el-table-column label="粉丝评论数"></el-table-column>
-                <el-table-column label="操作"></el-table-column>
+                <el-table-column prop="title"  width="600" label="标题"></el-table-column>
+                <el-table-column  :formatter="formatterBool"  prop="comment_status" label="评论状态"></el-table-column>
+                <el-table-column prop="total_comment_count" label="总评论数"></el-table-column>
+                <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
+                <el-table-column label="操作">
+                  <template slot-scope="obj">
+                    <el-button type="text">修改</el-button>
+                    <el-button type="text">{{obj.row.comment_status ? '关闭评论' : '打开'}}</el-button>
+                  </template>
+                </el-table-column>
                 </el-table>
             </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      list: []
+    }
+  },
+  methods: {
+    getcomment () {
+      this.$axios({
+        url: '/articles', params: { response_type: 'comment' }
+      }).then(result => {
+        this.list = result.data.results
+      })
+    },
+    formatterBool (row, column, cellValue, index) {
+      // row 当前数据
+      //  column 当前列的属性
+    //   debugger
+      return cellValue ? '正常' : '关闭'
+    }
+  },
+  created () {
+    this.getcomment()
+  }
 }
 </script>
 
